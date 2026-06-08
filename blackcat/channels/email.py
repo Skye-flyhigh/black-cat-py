9937d5af1db3ba649caa1977a2f391e02f13d56c
Flávio Veloso Soares <flaviovs@magnux.com>
2026-06-08 15:44:13 -0700

    style(email): fix import order via ruff

--- blackcat/channels/email.py
+++ blackcat/channels/email.py
@@ -17,7 +17,6 @@ from email.parser import BytesParser
 from email.utils import parseaddr
 from fnmatch import fnmatch
 from pathlib import Path
-from dataclasses import dataclass
 from typing import Any, Literal
 
 from loguru import logger
